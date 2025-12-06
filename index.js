a936451163e52d5ee4abcb56428d02602d254ed2
mkhshohud99 <mkhshohud1999@gmail.com>
2025-12-06 17:21:57 +0600

    Add service added

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const port = 3000;
+const port = 5000;
 
 
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 
 
@@ -25,6 +26,43 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         await client.connect();
+
+        const database = client.db('petPaw');
+        const petServices = database.collection('services');
+
+        //save services to DB
+
+        app.post('/services', async(req, res)=>{
+            const data = req.body;
+            const date = new Date();
+            data.createdAt = date;
+            console.log(data);
+            
+            console.log(data);
+            const result = await petServices.insertOne(data);
+            res.send(result)
+            
+
+        })
+
+        //Get a single service
+
+        app.get('/services/:id', async(req, res)=>{
+            const id = req.params;
+            // console.log(id);
+            const query = {_id: new ObjectId(id)}
+            const result = await petServices.findOne(query);
+            res.send(result)
+            
+        })
+
+        //Get service from DB
+
+        app.get('/services', async(req, res)=>{
+            const result = await petServices.find().toArray();
+            res.send(result);
+        })
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
